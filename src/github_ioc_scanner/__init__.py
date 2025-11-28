106f68561799d67b85a9af940faa1af6ce4df75e
Christian Herweg <christian.herweg@otto.de>
2025-11-28 15:57:14 +0100

    chore: Bump version to 1.7.0

--- src/github_ioc_scanner/__init__.py
+++ src/github_ioc_scanner/__init__.py
@@ -1,6 +1,6 @@
 """GitHub IOC Scanner - A tool to scan GitHub repositories for compromised packages."""
 
-__version__ = "1.5.4"
+__version__ = "1.7.0"
 
 from .ioc_loader import IOCLoader, IOCLoaderError, IOCDirectoryNotFoundError, IOCFileError
 from .models import (
